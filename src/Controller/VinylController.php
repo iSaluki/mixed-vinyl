c19a9a24f71b4dce6ebf9c783d043ac532411214
Seth MB <sethmb@pm.me>
2023-07-18 11:06:46 +0100

    added twig templating

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -5,13 +5,24 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class VinylController {
+class VinylController extends AbstractController{
 
     #[Route('/')]
     public function homepage(): Response {
 
-        return new Response('Title: PB and Jams');
+        $tracks = [
+                ['song' => 'It\'s raining beans', 'artist' => 'Master of the Beans'],
+                ['song' => 'I kissed a bean and I liked it', 'artist' => 'Toxic Beans'],
+                ['song' => 'I\'m gonna make a bean out of you', 'artist' => 'Unbean'],
+                ['song' => "It's looking like a white Christmas", 'artist' => 'Michael Beanly'],
+            ];
+            
+        return $this->render('vinyl/homepage.html.twig', [
+                'title' => "PB and Jams",
+                'tracks' => $tracks,
+            ]);
     }
 
     #[Route('/browse/{slug}')]
